63cab4894467b110c194b0c889d007ecffc48d33
David Cosgrove <dcosgrove@paperlesspost.com>
2016-02-24 14:31:28 -0500

    Added S3 file downloader, now pulls down files from bucket to disk

---
+++ download.js
@@ -0,0 +1,64 @@
+var fs = require('fs');
+var aws = require('aws-sdk');
+var _ = require('lodash');
+var Promise = require('bluebird');
+var mkdirp = require('mkdirp');
+
+var config = require('./config');
+
+// credentials should be in env:
+// AWS_ACCESS_KEY_ID and  AWS_SECRET_ACCESS_KEY
+var s3 = new aws.S3();
+
+// make sure our destination directory exists on startup
+
+var DEST_DIR = './downloads/';
+mkdirp.sync(DEST_DIR);
+
+// pull the list of filenames that we want to download
+var listFiles = function() {
+
+  return new Promise(function(resolve, reject) {
+    s3.listObjects({
+      Bucket: config.s3.bucket
+    }, function(err, data) {
+      if(err) {
+        return reject(err);
+      }
+      
+      var files = data.Contents.map(function(item) {
+        return item.Key;
+      });
+
+      resolve(files);
+    });
+  });
+};
+
+// get the file contents
+var fetchFile = function(file) {
+
+  return new Promise(function(resolve, reject) {
+    s3.getObject({
+      Bucket: config.s3.bucket,
+      Key: file
+    }, function(err, data) {
+      if(err) {
+        return reject(err);
+      }
+
+      resolve(data.Body);
+    });
+  });
+};
+
+// write this thing to disk
+var saveFile = function(file, buffer) {
+  return Promise.promisify(fs.writeFile)(DEST_DIR + file, buffer);
+};
+
+module.exports = {
+  listFiles: listFiles,
+  fetchFile: fetchFile,
+  saveFile: saveFile
+};
